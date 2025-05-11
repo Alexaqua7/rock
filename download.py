63a500e9002900221009623c3e25a984aa7533a5
Alexaqua7 <sdh582@naver.com>
2025-05-11 08:25:31 +0000

    :test_tube: InterImage-XL KFold 기능 추가

--- download.py
+++ download.py
@@ -6,3 +6,8 @@ tt_model_name = "OpenGVLab/internimage_xl_22kto1k_384"
 local_dir = './weights/OpenGVLab/internimage_xl_22kto1k_384'
 os.makedirs(local_dir, exist_ok=True)
 download_dir = snapshot_download(repo_id=tt_model_name, local_dir=local_dir)
+
+tt_model_name = "OpenGVLab/internimage_l_22kto1k_384"
+local_dir = './weights/OpenGVLab/internimage_l_22kto1k_384'
+os.makedirs(local_dir, exist_ok=True)
+download_dir = snapshot_download(repo_id=tt_model_name, local_dir=local_dir)
